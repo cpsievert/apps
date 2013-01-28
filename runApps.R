707a999c39db660fb74a9255105cda665978beb7
Carson Sievert <cpsievert1@gmail.com>
2013-01-28 14:53:11 -0600

    all "extensions" as orignial example

---
+++ runApps.R
@@ -0,0 +1,17 @@
+library(shiny)
+
+#local "Hello Shiny!"
+setwd("~/Desktop/shinyTalk/01_hello")
+runApp()
+
+#first 'extend' example
+setwd("~/Desktop/shinyTalk/extend1")
+runApp()
+
+#second 'extend' example
+setwd("~/Desktop/shinyTalk/extend2")
+runApp()
+
+#third 'extend' example
+setwd("~/Desktop/shinyTalk/extend3")
+runApp()
