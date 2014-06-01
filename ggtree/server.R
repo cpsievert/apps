bdba1e5829da3144359079a26725680ede1ee0ea
Anonymous@example.com <cpsievert1@gmail.com>
2014-06-01 16:33:22 -0500

    Add moar to ggtree

--- ggtree/server.R
+++ ggtree/server.R
@@ -3,21 +3,22 @@ library(ggplot2)
 library(shinyAce)
 
 # recursive approach! http://stackoverflow.com/questions/12818864/how-to-write-to-json-with-children-from-r
-makeList <- function(x){
-  if (ncol(x) > 2){
+makeList <- function(x) {
+  idx <- is.na(x[,2])
+  if (ncol(x) > 2 && sum(idx) != nrow(x)){
     listSplit <- split(x[-1], x[1], drop=T)
     lapply(names(listSplit), function(y){list(name = y, children = makeList(listSplit[[y]]))})
-  } else{
-    lapply(seq(nrow(x[1])), function(y){list(name = x[,1][y], value = x[,2][y])})
+  } else {
+    nms <- x[,1]
+    lapply(seq_along(nms), function(y){list(name = nms[y], value = x[,"value"][y])})
   }
 }
 
+# thanks Jeroen http://stackoverflow.com/questions/19734412/flatten-nested-list-into-1-deep-list
+renquote <- function(l) if (is.list(l)) lapply(l, renquote) else enquote(l)
 
 gg2tree <- function(gg) {
-  built <- ggplot_build(gg)
-  # thanks Jeroen http://stackoverflow.com/questions/19734412/flatten-nested-list-into-1-deep-list
-  renquote <- function(l) if (is.list(l)) lapply(l, renquote) else enquote(l)
-  la <- lapply(unlist(renquote(built)), eval)
+  la <- lapply(unlist(renquote(gg)), eval)
   # capture the output of the list values as one long character string
   vals <- lapply(la, function(x) paste(utils::capture.output(x), collapse = "<br>"))
   #names(vals) <- gsub("\\.", "_", names(vals))
@@ -34,23 +35,19 @@ shinyServer(function(input, output, session) {
   
   output$ggplot <- renderPlot({
     input$send
-    
     isolate({
-      p <- eval(parse(text = input$code))
-      print(p)
+      print(eval(parse(text = input$code)))
     })
-    
   })
   
-  output$dat <- reactive({
+  output$d3 <- reactive({
     input$send
-    
     isolate({
       p <- eval(parse(text = input$code))
-      return(gg2tree(p))
     })
-    
+    return(list(root = gg2tree(p), layout = input$d3layout))
   })
+  
   
   output$console <- renderPrint({
     input$send
--- ggtree/ui.R
+++ ggtree/ui.R
@@ -2,9 +2,10 @@ library(shiny)
 library(shinyAce)
 
 shinyUI(fluidPage(
-  titlePanel("Visualizaing ggplot2 internals"),
-  tags$h4('Made with love by', tags$a(href = "http://cpsievert.github.io/about.html", 'Carson Sievert'),
-          tags$a(href = "", 'See here'), 'for source code'),
+  titlePanel("Visualizing ggplot2 internals"),
+  tags$h4('See', tags$a(href = "http://cpsievert.github.io/", 'here'), 'for accompanying post and',
+          tags$a(href = "https://github.com/cpsievert/shiny_apps/tree/master/ggtree", 'here'), 
+          'for source code'),
              
   tags$head(
     tags$script(type="text/javascript", src = "d3.v3.js"),
@@ -15,12 +16,18 @@ shinyUI(fluidPage(
   
   fluidRow(
     column(width = 6,
-           HTML("<div id=\"dat\" class=\"shiny-scatter-output\"><svg /></div>") 
+           selectInput("d3layout", "Choose a layout:", 
+                       choices = c("Radial" = "radial",
+                                   "Collapsed" = "collapse",
+                                   "Cartesian" = "cartesian")),
+           HTML("<div id=\"d3\" class=\"d3plot\"><svg /></div>")
     ),
     column(width = 6,
       aceEditor("code", 
-                value="ggplot(mtcars, aes(mpg, wt)) + \n geom_point(colour='grey50', size = 4) + \n geom_point(aes(colour = cyl))",
-                mode = "r", theme = "chrome", height = "50px", fontSize = 10),
+                value="# Enter code to generate a ggplot here \n# Then click 'Send Code' when ready
+p <- ggplot(mtcars, aes(mpg, wt)) + \n geom_point(colour='grey50', size = 4) + \n geom_point(aes(colour = cyl)) + facet_wrap(~am, nrow = 2)
+# Visualize the 'built' version -- this is optional\nggplot_build(p)",
+                mode = "r", theme = "chrome", height = "100px", fontSize = 10),
       actionButton("send", "Send code"),
       plotOutput(outputId = "ggplot")
     )
