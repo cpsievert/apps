a6e54cbffc15a52c38ddb916c2e452619e2bb770
Anonymous@example.com <cpsievert1@gmail.com>
2015-01-17 20:58:00 -0600

    hrm

--- pitchRx2/ui.R
+++ pitchRx2/ui.R
@@ -1,3 +1,6 @@
+# NOTE TO SELF: Have the app update the database as a nightly CRON job!
+dates <- as.Date(substr(gids, 5, 14), format = "%Y_%m_%d")
+
 shinyUI(fluidPage(
   tabsetPanel(id = "tabs",
     wellPanel(
@@ -11,7 +14,7 @@ shinyUI(fluidPage(
       selectizeInput("pitcher", "Choose a pitcher:", 
                   choices = c("Any pitcher" = "any", player.names),
                   selected = "Mariano Rivera"),
-      selectizeInput("batter", "Choose a batter:", 
+      selectizeInput("batter", "Choose a batter:",
                   choices = c("Any batter" = "any", player.names)),
       br(),
       actionButton("query", "Send Query"), 
