31861f6353834c3b1645aaf14aa2ae479d505c48
Carson Sievert <cpsievert1@gmail.com>
2015-04-09 11:51:06 -0500

    Add zip script

---
+++ zip-script.R
@@ -0,0 +1,8 @@
+# devtools::install_github("ropensci/git2r")
+library(git2r)
+tmp <- tempfile()
+dir.create(tmp)
+repo <- clone("https://github.com/cpsievert/shiny_apps.git", tmp)
+apps <- file.path(tmp, c("ggtree", "LDAelife"))
+zip("cpsievert-apps.zip", apps)
+unlink(tmp)
