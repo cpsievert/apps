36cf7c3431972df14d46bb87bea22c8ad8ae1815
Carson Sievert <cpsievert1@gmail.com>
2015-12-06 14:55:27 +1100

    use tourbrush package

--- 3Dnormal/app.R
+++ 3Dnormal/app.R
@@ -1,10 +1,8 @@
+library(tourbrush)
 library(mvtnorm)
-
 s <- matrix(c(1, .5, .5, .5, 1, .5, .5, .5, 1), ncol = 3)
 m <- rmvnorm(500, sigma = s)
-d <- dmvnorm(m)
-
-dat <- data.frame(x = m[, 1], y = d)
-
-source("run_tour.R")
-run_tour(dat, m)
+# take 1D slice of the 3D density
+d <- setNames(data.frame(m), c("x", "y", "z"))
+d$density <- dmvnorm(m)
+tourbrush(d)
