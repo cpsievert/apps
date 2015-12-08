30b44b36d191b6d989452ea4b28f467573ee2ded
Carson Sievert <cpsievert1@gmail.com>
2015-12-08 14:06:20 +1100

    use tips data

--- brush2plotly/app.R
+++ brush2plotly/app.R
@@ -4,29 +4,35 @@ library(ggplot2)
 library(dplyr)
 library(plotly)
 
+data(tips, package = "reshape2")
+
 ui <- fluidPage(
-  plotOutput("scatter1", brush = brushOpts("brush", direction = "xy")),
-  plotlyOutput("plot1")
+  sidebarPanel(
+    plotOutput("scatter1", brush = brushOpts("brush", direction = "xy"))
+  ),
+  mainPanel(
+    plotlyOutput("plot1")
+  )
 )
 
 server <- function(input, output, session) {
   output$scatter1 <- renderPlot({
-    ggplot(iris, aes(Sepal.Length, Sepal.Width, color = Species)) + 
-      geom_point()
+    ggplot(tips, aes(total_bill, tip, color = day)) + 
+      geom_point() + theme(legend.position = "bottom")
   })
   
   output$plot1 <- renderPlotly({
-    p <- iris %>%
-      count(Species) %>%
-      plot_ly(x = Species, y = n, opacity = 0.5, type = "bar") %>%
+    p <- tips %>%
+      count(day) %>%
+      plot_ly(x = day, y = n, opacity = 0.5, type = "bar") %>%
       layout(barmode = "overlay", showlegend = FALSE)
     if (!is.null(input$brush)) { 
       br <- input$brush
-      s <- iris %>%
-        filter(br$xmin <= Sepal.Length, Sepal.Length <= br$xmax) %>%
-        filter(br$ymin <= Sepal.Width, Sepal.Width <= br$ymax) %>%
-        count(Species)
-      p <- add_trace(p, x = Species, y = n, data = s)
+      s <- tips %>%
+        filter(br$xmin <= total_bill, total_bill <= br$xmax) %>%
+        filter(br$ymin <= tip, tip <= br$ymax) %>%
+        count(day)
+      p <- add_trace(p, x = day, y = n, data = s)
     }
     p
   })
